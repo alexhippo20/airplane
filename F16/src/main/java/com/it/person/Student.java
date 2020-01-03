77f7cdfeba2a94929c42ffc7031dd1001577f4ed
Alex_glb <2638402643@qq.com>
2020-01-03 21:09:35 +0800

    student1

--- F16/src/main/java/com/it/person/Student.java
+++ F16/src/main/java/com/it/person/Student.java
@@ -3,7 +3,7 @@ package com.it.person;
 public class Student {
     public static void main(String[] args) {
         System.out.println("I am not a student any more?");
-        int i = 2;
+        int i = 3;
         System.out.println(++i);
     }
 }
